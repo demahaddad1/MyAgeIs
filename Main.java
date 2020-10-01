4637940165682335dd21785be275b2c64c715c9e
demahaddad1 <dema.haddad@patelhs.org>
2020-10-01 01:47:23 +0000

    I declared and initialized the age, birthday and todaysdate int variables, then i addded an if else statement that states if today = my birthday i add one to my age and if today doesnt equal my birthday it stays the same

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}
+    int age = 15;
+    int birthday = 1112;
+    int todaysdate = 0921;
+
+    if (birthday = todaysdate) {
+      // i am + 15 + years old
+
+       System.out.println("i am 16 years old");
+
+    }else{
+      // i am still + 15 + years old
+
+        System.out.println("i am 15 years old");
+
+
+
+
+
+ 
